6d19d5d4f049262df74ac89eb3f173388e0888ca
Igor Soloydenko <soloydenko@gmail.com>
2016-07-30 16:37:29 -0700

    Add tests for DictionaryExtensions

---
+++ Peppermint.Tests/Dictionaries/DictionaryExtensionsTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Peppermint.Dictionaries;
+using Xunit;
+
+namespace Peppermint.Tests.Dictionaries
+{
+    public class DictionaryExtensionsTests
+    {
+        [Fact]
+        public void NullToEmptyReturnsOriginalForNonNullDictionary()
+        {
+            // Arrange
+            var ints = new Dictionary<int, int>
+            {
+                { 1, 1 }
+            };
+
+            // Act
+            var result = ints.NullToEmpty();
+
+            // Assert
+            Assert.Equal(result, ints);
+        }
+
+        [Fact]
+        public void NullToEmptyReturnsEmptyForNullDictionary()
+        {
+            // Arrange
+            Dictionary<int, int> ints = null;
+
+            // Act
+            var result = ints.NullToEmpty();
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(0, result.Count);
+        }
+    }
+}
