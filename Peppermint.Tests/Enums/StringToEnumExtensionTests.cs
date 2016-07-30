ab65571d772a5d83c86a883965b09881a57dae6b
Igor Soloydenko <soloydenko@gmail.com>
2016-07-30 15:38:10 -0700

    StringToEnumExtensionTests

---
+++ Peppermint.Tests/Enums/StringToEnumExtensionTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Peppermint.Enums;
+using Xunit;
+
+namespace Peppermint.Tests.Enums
+{
+    public class StringToEnumExtensionTests
+    {
+        [Theory]
+        [InlineData("ValueA", TestEnum.ValueA)]
+        [InlineData("valuea", TestEnum.ValueA)]
+        [InlineData("ValueB", TestEnum.ValueB)]
+        [InlineData("valueb", TestEnum.ValueB)]
+        public void CanParseCorrectValues(string stringValue, TestEnum expected)
+        {
+            // Arrange
+            // Act
+            var result = stringValue.Parse<TestEnum>();
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(null, typeof(ArgumentNullException))]
+        [InlineData("", typeof(ArgumentException))]
+        [InlineData("noSuchValue", typeof(ArgumentException))]
+        public void ThrowsExceptionOnIncorrectValue(string stringValue, Type exceptionType)
+        {
+            // Arrange
+            // Act
+            var e = Assert.Throws(exceptionType, () => stringValue.Parse<TestEnum>());
+            
+            // Assert
+            Assert.Equal(exceptionType, e.GetType());
+        }
+    }
+
+    public enum TestEnum
+    {
+        ValueA,
+        ValueB
+    }
+}
