a995da7c066e9ce0648d4d82739cd6d98d508f17
Igor Soloydenko <soloydenko@gmail.com>
2016-07-30 16:25:09 -0700

    Add ArrayExtensionsTests

---
+++ Peppermint.Tests/Arrays/ArrayExtensionsTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using Peppermint.Arrays;
+using Xunit;
+
+namespace Peppermint.Tests.Arrays
+{
+    public class ArrayExtensionsTests
+    {
+        [Fact]
+        public void NullToEmptyReturnsOriginalForNonNullArray()
+        {
+            // Arrange
+            var ints = new[] {1, 2, 3};
+
+            // Act
+            var result = ints.NullToEmpty();
+
+            // Assert
+            Assert.True(result.SequenceEqual(ints));
+        }
+
+        [Fact]
+        public void NullToEmptyReturnsEmptyForNullArray()
+        {
+            // Arrange
+            int[] ints = null;
+
+            // Act
+            var result = ints.NullToEmpty();
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(0, result.Length);
+        }
+    }
+}
