5f4a87078639138f4f7149f74243cd9440b1db4f
Igor Soloydenko <soloydenko@gmail.com>
2016-07-30 11:40:33 -0700

    Modify Sequence implementation

--- Lists/Sequence.cs
+++ Lists/Sequence.cs
@@ -5,22 +5,29 @@ namespace Peppermint.Lists
 {
     public class Sequence
     {
-        public static Func<int, int, int, int> Natural = (min, max, current) => current + 1;
+        public static Func<int, int, int> Natural = (@base, current) => current + 1;
 
-        public static Func<int, int, int, int> NonNegative = (min, max, current) => current;
+        public static Func<int, int, int> NonNegative = (@base, current) => current;
 
-        public static Func<int, int, int, int> Ceil = (min, max, current) => current - max / 2;
+        public static Func<int, int, int> Ceil = (@base, current) => @base + current;
 
-        public static Func<int, int, int, int> ZeroPositiveNegative = (min, max, current) => current * (current % 2 == 1 ? 1 : -1);
+        public static Func<int, int, int> ZeroPositiveNegative = (@base, current) => @base + current * (current % 2 == 1 ? 1 : -1);
 
-        public static List<T> ListWithoutDuplicates<T>(
-            int elementNumber,
-            Func<int, int, int, T> sequenceItemGenerator)
+        public static IEnumerable<T> ListWithoutDuplicates<T>(
+            Func<int, int, T> sequenceItemGenerator,
+            int? baseOverride = null,
+            int? stepOverride = null)
         {
-            var result = new List<T>();
-            for (var count = 0; count < elementNumber; count++)
-                result.Add(sequenceItemGenerator(0, elementNumber - 1, count));
-            return result;
+            var @base = baseOverride ?? 0;
+            var step = stepOverride ?? 1;
+
+            var current = @base;
+            while (true)
+            {
+                var item = sequenceItemGenerator(@base, current);
+                current += step;
+                yield return item;
+            }
         }
     }
 }
