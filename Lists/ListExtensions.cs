37ccf44e9eeb33d54c1eb664bb8f4799a304923c
Igor Soloydenko <soloydenko@gmail.com>
2016-07-29 23:26:19 -0700

    Add list extensions and sequence generator

---
+++ Lists/ListExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Peppermint.Lists
+{
+    public static class ListExtensions
+    {
+        public static void Shuffle<T>(this IList<T> collection, int swapsNumber = -1, int seed = 1)
+        {
+            var random = new Random(seed);
+            var elementNumber = collection.Count;
+
+            if (swapsNumber == -1) swapsNumber = elementNumber * 3;
+
+            for (var swaps = 1; swaps <= swapsNumber; swaps++)
+                SwapAt(collection, random.Next(elementNumber), random.Next(elementNumber));
+        }
+
+        public static void SwapAt<T>(this IList<T> collection, int index1, int index2)
+        {
+            var temp = collection[index1];
+            collection[index1] = collection[index2];
+            collection[index2] = temp;
+        }
+    }
+}
---
+++ Lists/Sequence.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Peppermint.Lists
+{
+    public class Sequence
+    {
+        public static Func<int, int, int, int> Natural = (min, max, current) => current + 1;
+
+        public static Func<int, int, int, int> NonNegative = (min, max, current) => current;
+
+        public static Func<int, int, int, int> Ceil = (min, max, current) => current - max / 2;
+
+        public static Func<int, int, int, int> ZeroPositiveNegative = (min, max, current) => current * (current % 2 == 1 ? 1 : -1);
+
+        public static List<T> ListWithoutDuplicates<T>(
+            int elementNumber,
+            Func<int, int, int, T> sequenceItemGenerator)
+        {
+            var result = new List<T>();
+            for (var count = 0; count < elementNumber; count++)
+                result.Add(sequenceItemGenerator(0, elementNumber - 1, count));
+            return result;
+        }
+    }
+}
