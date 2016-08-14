6ed57cf74dc9446942da453c099951a4767422ec
ANOTHER GUY <another-guy@users.noreply.github.com>
2016-08-14 00:01:47 -0700

    String extensions (#4)

---
+++ Peppermint.Tests/PrimitiveTypes/StringExtensionsTest.cs
@@ -0,0 +1,29 @@
+﻿namespace Peppermint.Tests.PrimitiveTypes
+{
+    using Xunit;
+    using Peppermint.PrimitiveTypes;
+
+    public class StringExtensionsTest
+    {
+        [Theory]
+        [InlineData(null, null)]
+        [InlineData("", "")]
+        [InlineData("a", "a")]
+        [InlineData("b", "b")]
+        [InlineData("ab", "ba")]
+        [InlineData("xz", "zx")]
+        [InlineData("abc", "cba")]
+        [InlineData("xyz", "zyx")]
+        public void ReverseWorks(string target, string expected)
+        {
+            // Arrange
+            var sut = target;
+
+            // Act
+            var result = sut.Reverse();
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
--- Peppermint/Collections.Generic/ArrayExtensions.cs
+++ Peppermint/Collections.Generic/ArrayExtensions.cs
@@ -6,5 +6,12 @@
         {
             return target ?? new T[0];
         }
+
+        public static void SwapAt<T>(this T[] target, int index1, int index2)
+        {   
+            var temp = target[index1];
+            target[index1] = target[index2];
+            target[index2] = temp;
+        }
     }
 }
