9d90ca4cf9433ad26eb81a7c06e1127d6d647546
Igor Soloydenko <soloydenko@gmail.com>
2016-08-13 23:58:01 -0700

    String extensions

---
+++ Peppermint/PrimitiveTypes/StringExtensions.cs
@@ -0,0 +1,26 @@
+﻿namespace Peppermint.PrimitiveTypes
+{
+    using Peppermint.Collections.Generic;
+
+    public static class StringExtensions
+    {
+        public static string Reverse(this string target)
+        {
+            if (target == null) return null;
+            if (target == "") return "";
+
+            var chars = target.ToCharArray();
+
+            var left = 0;
+            var right = chars.Length - 1;
+            while (left < right)
+            {
+                chars.SwapAt(left, right);
+
+                left += 1;
+                right -= 1;
+            }
+            return new string(chars);
+        }
+    }
+}
