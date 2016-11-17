0ae9431eb1f55f56a7fca96f4a39c3b7ca379c28
Igor Soloydenko <soloydenko@gmail.com>
2016-11-16 16:08:08 -0800

    Add FlexProject and tests

--- Peppermint/Collections.Generic/IEnumerableExtensions.cs
+++ Peppermint/Collections.Generic/IEnumerableExtensions.cs
@@ -1,5 +1,6 @@
 ﻿namespace Peppermint.Collections.Generic
 {
+    using System;
     using System.Collections.Generic;
 
     public static class IEnumerableExtensions
@@ -7,6 +8,18 @@
         public static IEnumerable<T> NullToEmpty<T>(this IEnumerable<T> target)
         {
             return target ?? new T[0];
+        }
+
+        public static IEnumerable<TResult> FlexProject<T, TResult>(
+            this IEnumerable<T> source,
+            Func<T, ProjectAs<TResult>> smartSelector)
+        {
+            foreach (T item in source)
+            {
+                var selectorApplicationResult = smartSelector(item);
+                if (selectorApplicationResult.DoProjectValue)
+                    yield return selectorApplicationResult.Value;
+            }
         }
     }
 }
