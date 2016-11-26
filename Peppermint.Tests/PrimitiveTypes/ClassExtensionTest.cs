6c3cf10866736d78c698682dbb893874a3939da8
Igor Soloydenko <soloydenko@gmail.com>
2016-11-25 21:35:12 -0800

    Add ClassExtensions with NullToEmpty()

---
+++ Peppermint.Tests/PrimitiveTypes/ClassExtensionTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Xunit;
+
+namespace Peppermint.Tests.PrimitiveTypes
+{
+    public class ClassExtensionTest
+    {
+        [Fact]
+        public void NullToEmptyCreatesNewObjectFromOriginalNull()
+        {
+            // Arrange
+            object sut = null;
+
+            // Act
+            sut = sut.NullToEmpty();
+
+            // Assert
+            Assert.NotNull(sut);
+        }
+
+        [Fact]
+        public void NullToEmptyReturnsOriginalObjectFromNonNull()
+        {
+            // Arrange
+            var sut = new object();
+            var hashCode = sut.GetHashCode();
+
+            // Act
+            sut = sut.NullToEmpty();
+
+            // Assert
+            Assert.NotNull(sut);
+            Assert.Equal(hashCode, sut.GetHashCode());
+        }
+    }
+}
---
+++ Peppermint/PrimitiveTypes/ClassExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace System
+{
+    public static class ClassExtensions
+    {
+        public static T NullToEmpty<T>(this T target)
+            where T: class, new()
+        {
+            return target ?? new T();
+        }
+    }
+}
