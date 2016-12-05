c9de1e37bb86b56d43d995e994c9f202adcd948d
Igor Soloydenko <soloydenko@gmail.com>
2016-12-05 08:36:21 -0800

    Add object::NullToException(...)

--- Peppermint/PrimitiveTypes/ClassExtensions.cs
+++ Peppermint/PrimitiveTypes/ClassExtensions.cs
@@ -3,15 +3,24 @@
     public static class ClassExtensions
     {
         public static T NullToNew<T>(this T target)
-            where T: class, new()
+            where T : class, new()
         {
             return target.NullTo(new T());
         }
 
         public static T NullTo<T>(this T target, T alternative)
-            where T: class
+            where T : class
         {
             return target ?? alternative;
+        }
+
+        public static T1 NullToException<T1, T2>(this T1 target, T2 exception)
+            where T1 : class
+            where T2 : Exception
+        {
+            if (target == null)
+                throw exception;
+            return target;
         }
     }
 }
