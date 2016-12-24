faa45f4df2f7013ebb1eb89748b07b4397b5f421
Igor Soloydenko <soloydenko@gmail.com>
2016-12-24 03:15:28 -0800

    Add string.WithSuffix() / string.WithPrefix()

--- Peppermint.Tests/PrimitiveTypes/StringExtensionsTest.cs
+++ Peppermint.Tests/PrimitiveTypes/StringExtensionsTest.cs
@@ -25,5 +25,56 @@
             // Assert
             Assert.Equal(expected, result);
         }
+
+
+        [Theory]
+        [InlineData("", "", "")]
+        [InlineData("", "suffix", "suffix")]
+        [InlineData("value", "-suffix", "value-suffix")]
+        [InlineData("value", "", "value")]
+        public void WithSuffixWorks(string original, string suffix, string result)
+        {
+            // Arrange
+            // Act
+            var actualResult = original.WithSuffix(suffix);
+
+            // Assert
+            Assert.Equal(result, actualResult);
+        }
+
+        [Fact]
+        public void WithSuffixThrowsExceptionForNullPrefix()
+        {
+            // Arrange
+            // Act
+            var caught = Assert.Throws<ArgumentNullException>(() => "value".WithSuffix(null));
+            // Assert
+            Assert.True(caught.Message.Contains("suffix"));
+        }
+
+        [Theory]
+        [InlineData("", "", "")]
+        [InlineData("", "prefix", "prefix")]
+        [InlineData("value", "prefix-", "prefix-value")]
+        [InlineData("value", "", "value")]
+        public void WithPrefixWorks(string original, string prefix, string result)
+        {
+            // Arrange
+            // Act
+            var actualResult = original.WithPrefix(prefix);
+
+            // Assert
+            Assert.Equal(result, actualResult);
+        }
+
+        [Fact]
+        public void WithPrefixThrowsExceptionForNullPrefix()
+        {
+            // Arrange
+            // Act
+            var caught = Assert.Throws<ArgumentNullException>(() => "value".WithPrefix(null));
+            // Assert
+            Assert.True(caught.Message.Contains("prefix"));
+        }
     }
 }
