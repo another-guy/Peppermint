426f89422df3decd850e9413644a670ac1cb969c
Igor Soloydenko <soloydenko@gmail.com>
2016-12-25 20:49:38 -0800

    Add Type::GetAllBaseClasses()

---
+++ Peppermint/PrimitiveTypes/TypeExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace System
+{
+    public static class TypeExtensions
+    {
+        // TODO Test this
+        public static IEnumerable<Type> GetAllBaseTypes(this Type type)
+        {
+            var currentType = type;
+            while (true)
+            {
+                currentType = currentType.GetTypeInfo().BaseType;
+
+                if (currentType == typeof(object))
+                    yield break;
+
+                yield return currentType;
+            }
+        }
+    }
+}
