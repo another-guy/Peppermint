5d4f6e9bac881d4d74ea3ab580e3ffd0a215dd42
Igor Soloydenko <soloydenko@gmail.com>
2016-12-25 20:49:38 -0800

    Add Type::GetAllBaseClasses()

---
+++ Peppermint.Tests/PrimitiveTypes/TypeExtensionsTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace Peppermint.Tests.PrimitiveTypes
+{
+    public class TypeExtensionsTest
+    {
+        public void GetAllBaseTypesWorks()
+        {
+            // Arrange
+            var type = typeof(GrandChildClass);
+            var expected = new[] {typeof(ChildClass), typeof(ParentClass)};
+            // Act
+            var allBaseTypes = type.GetAllBaseTypes();
+            // Assert
+            Assert.True(expected.SequenceEqual(allBaseTypes));
+        }
+    }
+
+    public interface ParentInterface { }
+    public interface ChildInterface : ParentInterface { }
+    public class ParentClass: ParentInterface { }
+    public class ChildClass : ParentClass { }
+    public class GrandChildClass : ChildClass { }
+    public enum DemoEnum { }
+    public struct DemoStruct { }
+}
