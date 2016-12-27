e0465b1ca1985dc768533825f95d0a4422bc69ea
Igor Soloydenko <soloydenko@gmail.com>
2016-12-26 19:02:47 -0800

    Add more tests, rename methods

--- Peppermint.Tests/PrimitiveTypes/TypeExtensionsTest.cs
+++ Peppermint.Tests/PrimitiveTypes/TypeExtensionsTest.cs
@@ -9,7 +9,7 @@ namespace Peppermint.Tests.PrimitiveTypes
     public class TypeExtensionsTest
     {
         [Fact]
-        public void GetAllBaseTypesWorks()
+        public void GetAllBaseTypes()
         {
             // Arrange
             var type = typeof(GrandChildClass);
@@ -28,12 +28,22 @@ namespace Peppermint.Tests.PrimitiveTypes
         [InlineData(typeof(ParentClass), typeof(ChildClass), false)]
         [InlineData(typeof(ChildClass), typeof(GrandChildClass), false)]
         [InlineData(typeof(ParentClass), typeof(GrandChildClass), false)]
+        [InlineData(typeof(GenericChildClass<ChildInterface>), typeof(GenericParentClass<ChildInterface>), true)]
 
-        public void IsStrictlyChildOfClassWorks(Type childType, Type parentType, bool expectedResult)
+        [InlineData(typeof(IList<int>), typeof(IList), false)]
+        [InlineData(typeof(IList<int>), typeof(ICollection<int>), true)]
+        [InlineData(typeof(IList<int>), typeof(IEnumerable<int>), true)]
+        [InlineData(typeof(IList<int>), typeof(IEnumerable), true)]
+        [InlineData(typeof(IList), typeof(ICollection), true)]
+        [InlineData(typeof(IList), typeof(IEnumerable), true)]
+        [InlineData(typeof(IList), typeof(IList<int>), false)]
+        [InlineData(typeof(GenericChildClass<ParentInterface>), typeof(GenericInterfaceWithTasOut<ParentInterface>), true)]
+        [InlineData(typeof(GenericChildClass<ChildInterface>), typeof(GenericInterfaceWithTasOut<ParentInterface>), true)]
+        public void IsChildTypeOf(Type childType, Type parentType, bool expectedResult)
         {
             // Arrange
             // Act
-            var actualResult = childType.IsStrictlyChildOfClass(parentType);
+            var actualResult = childType.IsChildTypeOf(parentType);
             // Assert
             Assert.Equal(expectedResult, actualResult);
         }
@@ -46,35 +56,8 @@ namespace Peppermint.Tests.PrimitiveTypes
         [InlineData(typeof(ChildClass), typeof(ParentClass), false)]
         [InlineData(typeof(GrandChildClass), typeof(ChildClass), false)]
         [InlineData(typeof(GrandChildClass), typeof(ParentClass), false)]
+        [InlineData(typeof(GenericParentClass<ChildInterface>), typeof(GenericChildClass<ChildInterface>), true)]
 
-        public void IsStrictlyParentClassOfWorks(Type parentType, Type childType, bool expectedResult)
-        {
-            // Arrange
-            // Act
-            var actualResult = parentType.IsStrictlyParentClassOf(childType);
-            // Assert
-            Assert.Equal(expectedResult, actualResult);
-        }
-
-        [Theory]
-        [InlineData(typeof(IList<int>), typeof(IList), false)]
-        [InlineData(typeof(IList<int>), typeof(ICollection<int>), true)]
-        [InlineData(typeof(IList<int>), typeof(IEnumerable<int>), true)]
-        [InlineData(typeof(IList<int>), typeof(IEnumerable), true)]
-        [InlineData(typeof(IList), typeof(ICollection), true)]
-        [InlineData(typeof(IList), typeof(IEnumerable), true)]
-        [InlineData(typeof(IList), typeof(IList<int>), false)]
-        public void IsStrictlyChildOfInterfaceWorks(Type childType, Type parentInterfaceType, bool expectedResult)
-        {
-            // Arrange
-            // Act
-            var actualResult = childType.IsStrictlyChildOfInterface(parentInterfaceType);
-            // Assert
-            Assert.Equal(expectedResult, actualResult);
-        }
-
-
-        [Theory]
         [InlineData(typeof(IList), typeof(IList<int>), false)]
         [InlineData(typeof(ICollection<int>), typeof(IList<int>), true)]
         [InlineData(typeof(IEnumerable<int>), typeof(IList<int>), true)]
@@ -82,22 +65,84 @@ namespace Peppermint.Tests.PrimitiveTypes
         [InlineData(typeof(ICollection), typeof(IList), true)]
         [InlineData(typeof(IEnumerable), typeof(IList), true)]
         [InlineData(typeof(IList<int>), typeof(IList), false)]
-        public void IsStrictlyParentInterfaceOfWorks(Type parentInterfaceType, Type childType, bool expectedResult)
+        public void IsParentTypeOf(Type parentType, Type childType, bool expectedResult)
         {
             // Arrange
             // Act
-            var actualResult = parentInterfaceType.IsStrictlyParentInterfaceOf(childType);
+            var actualResult = parentType.IsParentTypeOf(childType);
+            // Assert
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Theory]
+        // Same as for IsCompatibleChildOfClass
+        [InlineData(typeof(ChildClass), typeof(object), true)]
+        [InlineData(typeof(ChildClass), typeof(ParentClass), true)]
+        [InlineData(typeof(GrandChildClass), typeof(ChildClass), true)]
+        [InlineData(typeof(GrandChildClass), typeof(ParentClass), true)]
+        [InlineData(typeof(ParentClass), typeof(ChildClass), false)]
+        [InlineData(typeof(ChildClass), typeof(GrandChildClass), false)]
+        [InlineData(typeof(ParentClass), typeof(GrandChildClass), false)]
+        [InlineData(typeof(GenericChildClass<ChildInterface>), typeof(GenericParentClass<ChildInterface>), true)]
+
+        [InlineData(typeof(IList<int>), typeof(IList), false)]
+        [InlineData(typeof(IList<int>), typeof(ICollection<int>), true)]
+        [InlineData(typeof(IList<int>), typeof(IEnumerable<int>), true)]
+        [InlineData(typeof(IList<int>), typeof(IEnumerable), true)]
+        [InlineData(typeof(IList), typeof(ICollection), true)]
+        [InlineData(typeof(IList), typeof(IEnumerable), true)]
+        [InlineData(typeof(IList), typeof(IList<int>), false)]
+        [InlineData(typeof(GenericChildClass<ParentInterface>), typeof(GenericInterfaceWithTasOut<ParentInterface>), true)]
+        [InlineData(typeof(GenericChildClass<ChildInterface>), typeof(GenericInterfaceWithTasOut<ParentInterface>), true)]
+        // Softer rules
+        [InlineData(typeof(GenericChildClass<>), typeof(GenericChildClass<>), true)]
+        [InlineData(typeof(GenericInterfaceWithTasOut<>), typeof(GenericInterfaceWithTasOut<>), true)]
+
+        [InlineData(typeof(GenericInterfaceWithTasOut<ChildInterface>), typeof(GenericInterfaceWithTasOut<>), true)]
+        [InlineData(typeof(GenericInterfaceWithTasOut<>), typeof(GenericInterfaceWithTasOut<ChildInterface>), false)]
+
+        [InlineData(typeof(GenericChildClass<ChildInterface>), typeof(GenericParentClass<>), true)]
+        [InlineData(typeof(GenericParentClass<>), typeof(GenericChildClass<ChildInterface>), false)]
+
+        [InlineData(typeof(GenericChildClass<ChildInterface>), typeof(GenericInterfaceWithTasOut<>), true)]
+        [InlineData(typeof(GenericInterfaceWithTasOut<>), typeof(GenericChildClass<ChildInterface>), false)]
+
+        [InlineData(typeof(GenericChildClass<string>), typeof(GenericChildClass<string>), true)]
+        [InlineData(typeof(GenericChildClass<string>), typeof(GenericChildClass<>), true)]
+        [InlineData(typeof(GenericChildClass<>), typeof(GenericChildClass<string>), false)]
+
+        [InlineData(typeof(IDictionary<,>), typeof(IDictionary<,>), true)]
+        [InlineData(typeof(IDictionary<string,int>), typeof(IDictionary<,>), true)]
+        [InlineData(typeof(IDictionary<,>), typeof(IDictionary<string, int>), false)]
+
+        [InlineData(typeof(IDictionary<int,string>), typeof(IDictionary<string, int>), false)]
+
+        [InlineData(typeof(Tuple<>), typeof(Tuple<,>), false)]
+        [InlineData(typeof(Tuple<,>), typeof(Tuple<,,>), false)]
+        [InlineData(typeof(Tuple<,>), typeof(Tuple<>), false)]
+        [InlineData(typeof(Tuple<,,>), typeof(Tuple<,>), false)]
+        [InlineData(typeof(Tuple<string, int, object>), typeof(Tuple<,>), false)]
+        [InlineData(typeof(Tuple<string, int, object>), typeof(Tuple<,,>), true)]
+        [InlineData(typeof(Tuple<,,>), typeof(Tuple<string, int, object>), false)]
+        [InlineData(typeof(Tuple<object, object, object>), typeof(Tuple<string, int, object>), false)]
+        public void IsChildTypeOfPossiblyOpenGeneric(Type childType, Type parentType, bool expectedResult)
+        {
+            // Arrange
+            // Act
+            var actualResult = childType.IsChildTypeOfPossiblyOpenGeneric(parentType);
             // Assert
             Assert.Equal(expectedResult, actualResult);
         }
     }
 
+    public interface GenericInterfaceWithTasOut<out T> { }
+    public class GenericParentClass<T> : GenericInterfaceWithTasOut<T> { }
+    public class GenericChildClass<T> : GenericParentClass<T> { }
+
     public interface ParentInterface { }
     public interface ChildInterface : ParentInterface { }
+
     public class ParentClass: ParentInterface { }
     public class ChildClass : ParentClass { }
     public class GrandChildClass : ChildClass { }
-    public enum DemoEnum { }
-    public struct DemoStruct { }
 }
-
