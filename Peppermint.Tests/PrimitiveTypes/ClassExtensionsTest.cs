4d8802eab1e47040be5ca132e8f2f0f16315b543
Igor Soloydenko <soloydenko@gmail.com>
2016-12-29 18:40:01 -0800

    Add UseAsArgFor<T>

--- Peppermint.Tests/PrimitiveTypes/ClassExtensionsTest.cs
+++ Peppermint.Tests/PrimitiveTypes/ClassExtensionsTest.cs
@@ -55,5 +55,57 @@ namespace Peppermint.Tests.PrimitiveTypes
 
             Assert.Same(target, result);
         }
+
+        [Fact]
+        public void UseAsArgForCanInvokeAction()
+        {
+            // Arrange
+            const string target = "some string";
+            object trackedObject = null;
+            Action<string> action = argument => trackedObject = argument;
+
+            // Act
+            target.UseAsArgFor(action);
+
+            // Assert
+            Assert.Equal(target, trackedObject);
+        }
+
+        [Fact]
+        public void UseAsArgThrowsOnNullAction()
+        {
+            // Arrange
+            const string target = "some string";
+            Action<string> action = null;
+
+            // Act, Assert
+            Assert.Throws<NullReferenceException>(() => target.UseAsArgFor(action));
+        }
+
+        [Fact]
+        public void UseAsArgForCanInvokeFunction()
+        {
+            // Arrange
+            const string target = "some string";
+            const string expectedResult = "SOME STRING";
+            Func<string, string> action = argument => argument.ToUpperInvariant();
+
+            // Act
+            var actualResult = target.UseAsArgFor(action);
+
+            // Assert
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Fact]
+        public void UseAsArgThrowsOnNullFunction()
+        {
+            // Arrange
+            const string target = "some string";
+            Func<string, string> action = null;
+
+            // Act, Assert
+            Assert.Throws<NullReferenceException>(() => target.UseAsArgFor(action));
+        }
     }
 }
--- Peppermint/PrimitiveTypes/ClassExtensions.cs
+++ Peppermint/PrimitiveTypes/ClassExtensions.cs
@@ -27,5 +27,16 @@ namespace System
                 throw exception;
             return target;
         }
+
+        public static void UseAsArgFor<T>(this T targetAsArgument, Action<T> action)
+        {
+            action.Invoke(targetAsArgument);
+        }
+
+        [Pure]
+        public static T2 UseAsArgFor<T1, T2>(this T1 targetAsArgument, Func<T1, T2> action)
+        {
+            return action(targetAsArgument);
+        }
     }
 }
